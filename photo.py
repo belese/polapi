a00ba0d961ffc0c30f845891e01e28974c098fcf
belese <besme2@gmail.com>
2017-01-10 14:12:20 +0100

    Update photo.py

--- photo.py
+++ photo.py
@@ -1,8 +1,12 @@
 import numpy as np
 import cv2
 
+
+EQUALIZEHIST = 1
+CLAHE = 2
+
 class polapi :
-   def gamma_correction(self,gamma) :
+   def gamma_correction(self,image,gamma) :
        # build a lookup table mapping the pixel values [0, 255] to
 	     # their adjusted gamma values
 	     invGamma = 1.0 / gamma
@@ -10,5 +14,10 @@ class polapi :
 		   for i in np.arange(0, 256)]).astype("uint8")
 	         # apply gamma correction using the lookup table
 	         return cv2.LUT(image, table)
+   
+  def equalize(self,image,mode) :
+		
+	
+	
    
       
