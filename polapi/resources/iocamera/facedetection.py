b14d0f43054ad836d9418552eaca741aa970d01c
belese <johan@solidbot.com>
2019-05-16 22:03:10 +0200

    fix

--- polapi/resources/iocamera/facedetection.py
+++ polapi/resources/iocamera/facedetection.py
@@ -14,7 +14,7 @@ class face_detection(lastframe) :
         self.onface = onface
         threading.Thread(target=self.run).start()
         self.res = (int(self.resolution[0]*FACTOR),int(self.resolution[1]*FACTOR))
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        self.clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 
     def run(self) :
         i=0
@@ -27,13 +27,13 @@ class face_detection(lastframe) :
              # create a CLAHE object (Arguments are optional).
             print ('clahe')
             gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
-            gray = clahe.apply(gray)                        
+            gray = self.clahe.apply(gray)                        
             print ('color')
             gray = cv2.resize(gray,self.res)
             print ('detect')
             faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=5)            
             print ('lookup for faces done',faces)
             if len(faces) > 0 :
-                faces =map(lambda(x) : [int(x[0]/FACTOR),int(x[1]/FACTOR),int(x[2]/FACTOR),int(x[3]*2/FACTOR)],faces)
+                faces =map(lambda x : [int(x[0]/FACTOR),int(x[1]/FACTOR),int(x[2]/FACTOR),int(x[3]*2/FACTOR)],faces)
                 img = Image.fromarray(frame)
                 self.onface(img,faces)
